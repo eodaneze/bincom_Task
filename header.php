cd7bd9699bfb85b182830b11ffdb3846aff70014
Daniel Ezeali <ezealiokechukwu1999@gmail.com>
2023-06-08 04:06:26 -0700

    designing the index page

--- header.php
+++ header.php
@@ -8,9 +8,19 @@
     <!--boostrap cdn link  -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <!--end of boostrap cdn link  -->
+
+    <!-- font-awesome cdn -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- end of font-awesome cdn -->
+
+    <!-- fevicon -->
+    <link rel="shortcut icon" href="./assets/img/fevicon.png" type="image/x-icon">
+    <!--end of fevicon -->
     <link rel="stylesheet" href="./assets/css/index.css">
 
-    <!--end of boostrap cdn link  -->
     <title>
         <?php
                if(isset($title)){
--- index.php
+++ index.php
@@ -1,55 +1,52 @@
 <?php
-  require_once('./model/connection.php');
-  $title = "Home";
-  require_once('./header.php');
-?>
-
-
-<body>
-    <div class="container border p-3 shadow">
-        <h4 class="text-center">Result for anocha north polling unit</h4>
-        <div class="table-responsive">
-            <table class="table borderless">
-                <tr>
-                    <th>S/N</th>
-                    <th>Party Name</th>
-                    <th>Party Result</th>
-                </tr>
-            </table>
-            <tbody>
-                <?php
-
-                  $sql = "SELECT * FROM announced_pu_results WHERE polling_unit_uniqueid = 8";
-                  $result = mysqli_query($con, $sql);
-                  $num = 1;
-                  while($row = mysqli_fetch_assoc($result)){
-     
-                  $score = $row['party_score'];
-                   $party_name = $row['party_abbreviation'];
-                 ?>
-                <tr>
-                    <td><?=$num++?></td>
-                    <td><?=$score?></td>
-                </tr>
-
-                <?php
- }
-
+   require_once('./model/connection.php');
+   $title = "HOME";
+   require_once('./header.php');
+   require_once('./navbar.php');
 
 ?>
-            </tbody>
+
+<body style="background-color: #38405A;">
+
+    <div class="all-div">
+        <div>
+            <div class="container mt-5">
+                <h5 class="text-center mb-4 text-uppercase text-white">Result for anocha north polling unit</h5>
+                <table class="table table-responsive table borderless bg-white border">
+                    <thead>
+                        <tr align="center">
+                            <th>S/N</th>
+                            <th>Party Name</th>
+                            <th>Party Result</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $sql = "SELECT * FROM announced_pu_results WHERE polling_unit_uniqueid = 8";
+                        $result = mysqli_query($con, $sql);
+                        $num = 1;
+                        
+                        while($row = mysqli_fetch_array($result)){
+                           $party_name = $row['party_abbreviation'];
+                           $score = $row['party_score'];
+
+                            ?>
+                        <tr align="center">
+                            <td><a href=""><?=$num++?></a></td>
+                            <td><?=$party_name?></td>
+                            <td><?=$score?></td>
+                        </tr>
+
+                        <?php 
+                        }
+                     ?>
+                    </tbody>
+                </table>
+            </div>
         </div>
-
     </div>
 </body>
 
-
-<!-- boostrap cdn script-->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
-</script>
-<!--end of boostrap cdn script-->
-
-
-
-</html>
+<?php
+ require_once('./footer.php')
+?>
